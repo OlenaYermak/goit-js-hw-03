697615f9f5284348b4380814947c280e35085a16
OlenaYermak <145419464+OlenaYermak@users.noreply.github.com>
2023-12-21 16:06:08 +0200

    add tasks

--- js/task-2.js
+++ js/task-2.js
@@ -1,8 +1,15 @@
 'use strict';
-function getShippingMessage(country, price, deliveryFee) {
-    let totalPrice = price + deliveryFee;
-    return `Shipping to ${country} will cost ${totalPrice} credits`;
-}
-console.log(getShippingMessage("Australia", 120, 50)); 
-console.log(getShippingMessage("Germany", 80, 20)); 
-console.log(getShippingMessage("Sweden", 100, 20));
+function makeArray(firstArray, secondArray, maxLength) {
+    const unitedArr = firstArray.concat(secondArray);
+
+     return unitedArr.length >= maxLength ? unitedArr.slice(0, maxLength) : unitedArr
+  
+ }
+
+
+console.log(makeArray(["Mango", "Poly"], ["Ajax", "Chelsea"], 3)); 
+console.log(makeArray(["Mango", "Poly", "Houston"], ["Ajax", "Chelsea"], 4)); 
+console.log(makeArray(["Mango"], ["Ajax", "Chelsea", "Poly", "Houston"], 3)); 
+console.log(makeArray(["Earth", "Jupiter"], ["Neptune", "Uranus"], 2)); 
+console.log(makeArray(["Earth", "Jupiter"], ["Neptune", "Uranus"], 4)); 
+console.log(makeArray(["Earth", "Jupiter"], ["Neptune", "Uranus", "Venus"], 0)); 
